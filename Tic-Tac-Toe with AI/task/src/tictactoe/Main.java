b8ecac6044a7a0e7cc5797bb78441072163ef313
Maikody <73773432+Maikody@users.noreply.github.com>
2021-01-03 18:16:53 +0100

    Editorial changes

--- Tic-Tac-Toe with AI/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe with AI/task/src/tictactoe/Main.java
@@ -12,19 +12,11 @@ public class Main {
         turn = turn.equals("X") ? "O" : "X";
     }
 
-//    public static void changePlayer(){
-//        if(player instanceof User){
-//            player =
-//        }
-//    }
-
-
-
     public static void main(String[] args) {
         while(true) {
             generateGameField();
 
-            System.out.println("Input command:");
+            System.out.println("Input command: [start/exit] [user/easy/medium/hard] [user/easy/medium/hard]");
             String startOrEnd, whoStarts, opponent;
             while (true) {
                 try {
@@ -54,31 +46,22 @@ public class Main {
     public static void gameLoop(Object[] players) {
         turn = "X";
         if (players[0] instanceof AI && players[1] instanceof AI) {
-            AI aiPlayer = (AI) players[0];
+            AI player1 = (AI) players[0];
             AI player2 = (AI) players[1];
             while (!checkStateOfTheGame()) {
-                aiPlayer.setAiSymbol(turn);
-                int[] computerMoves = aiPlayer.move(gameField);
-                markSymbol(computerMoves[0], computerMoves[1]);
+                player1.setAiSymbol(turn);
+                int[] computer1Moves = player1.move(gameField);
+                markSymbol(computer1Moves[0], computer1Moves[1]);
                 printGameField();
-//                if (checkStateOfTheGame()) {
-//                    return;
-//                }
+                if (checkStateOfTheGame()) {
+                    return;
+                }
                 swapTurn();
-//                int[] computer1Moves = player1.move(gameField, turn);
-//                markSymbol(computer1Moves[0], computer1Moves[1]);
-//                printGameField();
-//                if (checkStateOfTheGame()) {
-//                    return;
-//                }
-//                swapTurn();
-//                int[] computer2Moves = player2.move(gameField, turn);
-//                markSymbol(computer2Moves[0], computer2Moves[1]);
-//                printGameField();
-//                if (checkStateOfTheGame()) {
-//                    return;
-//                }
-//                swapTurn();
+                player2.setAiSymbol(turn);
+                int[] computer2Moves = player2.move(gameField);
+                markSymbol(computer2Moves[0], computer2Moves[1]);
+                printGameField();
+                swapTurn();
             }
         } else if (players[0] instanceof User && players[1] instanceof AI) {
             printGameField();
@@ -98,9 +81,7 @@ public class Main {
                 int[] computerMoves = player2.move(gameField);
                 markSymbol(computerMoves[0], computerMoves[1]);
                 printGameField();
-                if (checkStateOfTheGame()) {
-                    return;
-                }
+
                 swapTurn();
             }
         } else if (players[0] instanceof AI && players[1] instanceof User) {
@@ -124,35 +105,16 @@ public class Main {
                 }
                 swapTurn();
             }
-        } else if (players[0] instanceof User && players[1] instanceof User){
+        } else if (players[0] instanceof User && players[1] instanceof User) {
             printGameField();
             User player = (User) players[0];
-//            User player2 = (User) players[1];
             while (!checkStateOfTheGame()) {
                 System.out.println("Enter the coordinates:");
                 int[] userMoves = player.move(gameField, scanner);
                 markSymbol(userMoves[0], userMoves[1]);
                 printGameField();
-//                if (checkStateOfTheGame()) {
-//                    return;
-//                }
+
                 swapTurn();
-//                System.out.println("Enter the coordinates:");
-//                int[] user1Moves = player1.move(gameField, scanner);
-//                markSymbol(user1Moves[0], user1Moves[1]);
-//                printGameField();
-//                if (checkStateOfTheGame()) {
-//                    return;
-//                }
-//                swapTurn();
-//                System.out.println("Enter the coordinates:");
-//                int[] user2Moves = player2.move(gameField, scanner);
-//                markSymbol(user2Moves[0], user2Moves[1]);
-//                printGameField();
-//                if (checkStateOfTheGame()) {
-//                    return;
-//                }
-//                swapTurn();
             }
         }
     }
@@ -212,27 +174,6 @@ public class Main {
 
 
     public static void markSymbol(int firstCoordinate, int secondCoordinate) {
-//        int xCounter = 0;
-//        int yCounter = 0;
-//
-//        for (int i = 1; i < 4; i++) {
-//            for (int j = 1; j < 7; j++) {
-//                if (gameField[i][j].equals("X")) {
-//                    xCounter++;
-//                } else if (gameField[i][j].equals("O")) {
-//                    yCounter++;
-//                }
-//            }
-//        }
-
-//        String symbol;
-//        if (xCounter == yCounter) {
-//            symbol = "X";
-//        } else {
-//            symbol = "O";
-//        }
-
-//        gameField[firstCoordinate][secondCoordinate * 2] = symbol;
         gameField[firstCoordinate][secondCoordinate * 2] = turn;
     }
 
